cfeaa987ab22f641af1e62f87d0e977947d10522
dnovobranovexceedteam <69138948+dnovobranovexceedteam@users.noreply.github.com>
2020-08-06 14:20:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,110 @@
-let todoList = [];
+const todoList = document.querySelector("#todoList");
+const input = document.querySelector("#input");
+const checkAll = document.querySelector("#checkAll");
 
-(function() {
-  document.querySelector('input').addEventListener('keydown', function(e) {
-    if (e.keyCode === 13) {
-      console.log(this.value);
+let taskList = [];
+
+// $(".text-span").click(() =>
+//   console.log("aaaaaaaaaaaaAAAAAAAAAAAaaaAAAAaaaaaaaaaaaaaaaaaa")
+// );
+
+function addItem(text) {
+  // $('#todoList').append(`
+  // <li class="">
+  // 	<input type="checkbox">
+  // 	<span>
+  // 		${text}
+  // 	</span>
+  // 	<span>
+  // 		X
+  // 	</span>
+  // </li>
+  // `)
+  let checkAll = document.createElement("input");
+  checkAll.type = "checkbox";
+  input.appendChild(checkAll);
+  checkAll.addEventListener("click", function () {});
+
+  $("#checkAll").click(function (event) {
+    if (this.checked) {
+      $(":checkbox").each(function () {
+        li.classList.toggle("done");
+
+        this.checked = true;
+      });
+    } else {
+      $(":checkbox").each(function () {
+        this.checked = false;
+        li.classList.toggle("done");
+      });
     }
   });
-})();
- let newTodo = {
-     todo: this.value,
-     check: false,
-     important: false
- };
 
- todoList.push(newTodo)
- function displayMessages();
- console.log(newTodo);
+  const li = document.createElement("li");
+  todoList.append(li);
 
- function displayMessages(){
- 
-    todoList.forEach(function(item, i){
-    console.log(item);
- }};
+  const bottomBlock = document.createElement("div");
+  li.append(bottomBlock);
+
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  li.appendChild(checkbox);
+  checkbox.addEventListener("click", function () {
+    li.classList.toggle("done");
+  });
+
+  let span = document.createElement("span");
+  span.innerHTML = text;
+  // span.classList.add('text-span')
+  this.value = "";
+  li.appendChild(span);
+
+  span.addEventListener("click", function func(e) {
+    let input = document.createElement("input");
+    console.log(this);
+    input.value = this.innerHTML;
+    this.innerHTML = "";
+    this.appendChild(input);
+	// $(input).focus()
+    // this.removeEventListener('click', func);
+
+    let self = this;
+    input.addEventListener("keydown", function (e) {
+      if (e.keyCode == 13) {
+        self.innerHTML = this.value;
+        changeItem(this.value);
+      }
+      //
+    //   document.getElementsByName("span").addEventListener("click", function () {
+    //     generateChars();
+    //   });
+    });
+  });
+
+  let remove = document.createElement("a");
+  remove.innerHTML = "X";
+  remove.href = " ";
+  li.appendChild(remove);
+  remove.addEventListener("click", function (event) {
+    li.parentElement.removeChild(li);
+
+    event.preventDefault();
+  });
+}
+let showAll = document.createElement("a");
+showAll.innerHTML = "All";
+
+input.addEventListener("keydown", function (e) {
+  if (e.keyCode == 13) {
+    addItem(input.value);
+
+    let newTodo = {
+      todo: input.value,
+      checked: false,
+    };
+    input.value = "";
+    taskList.push(newTodo);
+    console.log(taskList);
+  }
+});
